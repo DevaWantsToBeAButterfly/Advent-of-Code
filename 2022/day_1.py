87fbc124c14ef2d8596622ffbffc118576eb3079
Deva <devawantstobeabutterfly@gmail.com>
2022-12-15 08:43:44 +0100

    Tried to make the code nicer, but I may or may not have written an abomination of a list comprehension one-liner

--- 2022/day_1.py
+++ 2022/day_1.py
@@ -1,21 +1,20 @@
 with open('.\data_files\day_1_input.txt') as input_file:
-    data = input_file.readlines()
+    calories_list = [int(line) if line != '\n' else 0 for line in input_file.readlines()]
 
 input_file.close()
 
 current_elf_calories = 0
-calories_totals = []
+elves_calorie_totals = []
 
-for line in data:
-    if line != '\n':
-        current_elf_calories += int(line)
+for line in calories_list:
+    if line:
+        current_elf_calories += line
 
     else:
-        calories_totals.append(current_elf_calories)
+        elves_calorie_totals.append(current_elf_calories)
         current_elf_calories = 0
 
-print('The Elf carrying the most calories has', max(calories_totals), 'calories')
+elves_calorie_totals.sort(reverse=True)
 
-calories_totals.sort(reverse=True)
-top_three_elves_calories = calories_totals[0] + calories_totals[1] + calories_totals[2]
-print('In total, the top three Elves are carrying', top_three_elves_calories, 'calories')
+print('The Elf carrying the most calories has', elves_calorie_totals[0], 'calories')
+print('In total, the top three Elves are carrying', sum(elves_calorie_totals[0:3]), 'calories')
