1e1d1909c0212d17cecaa6002a36cf69ecea5ee2
Deva <devawantstobeabutterfly@gmail.com>
2022-12-16 20:31:28 +0100

    Wrote a generic function to read input from a passed file path and return a list of lines as strings

---
+++ 2022/generic_functions.py
@@ -0,0 +1,6 @@
+def read_input(filepath):
+    with open(filepath) as input_file:
+        input_list = [line.replace('\n','') for line in input_file.readlines()]
+
+    input_file.close()
+    return input_list
