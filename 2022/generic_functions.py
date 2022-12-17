d86af64359367ae70ad80a591ed759d4b973cfc3
Deva <devawantstobeabutterfly@gmail.com>
2022-12-17 20:49:05 +0100

    Added a read_input() that expects a filepath and returns a single string containing the input

--- 2022/generic_functions.py
+++ 2022/generic_functions.py
@@ -4,3 +4,11 @@ def read_lines(filepath):
 
     input_file.close()
     return input_list
+
+
+def read_input(filepath):
+    with open(filepath) as input_file:
+        input_data = input_file.read()
+
+    input_file.close()
+    return input_data
