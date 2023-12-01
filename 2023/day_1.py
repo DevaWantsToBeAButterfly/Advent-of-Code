24e99ab274a7598f6bf8f9b393600c1493279d66
Deva <devawantstobeabutterfly@gmail.com>
2023-12-01 19:47:18 +0100

    Removed '0' and 'zero' from lists as those are actually not valid numbers to be searched

--- 2023/day_1.py
+++ 2023/day_1.py
@@ -1,7 +1,7 @@
 from aocd import get_data, submit
 
-numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
-words = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+numbers = ['1', '2', '3', '4', '5', '6', '7', '8', '9']
+words = ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
 numbers_and_words = numbers + words
 words_to_nums = {'zero': '0', 'one': '1', 'two': '2', 'three': '3', 'four': '4', 'five': '5', 'six': '6',
                  'seven': '7', 'eight': '8', 'nine': '9'}
