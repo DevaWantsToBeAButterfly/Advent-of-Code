0b2780a7e60eb9badc6f10f060e2b27283bfce54
Deva <devawantstobeabutterfly@gmail.com>
2023-12-06 15:58:53 +0100

    Prettified things

--- 2023/day_6.py
+++ 2023/day_6.py
@@ -5,11 +5,10 @@ race_list = [{'Time': 42, 'Distance': 284, 'Ways to beat': 0}, {'Time': 68, 'Dis
              {'Time': 69, 'Distance': 1122, 'Ways to beat': 0}, {'Time': 85, 'Distance': 1341, 'Ways to beat': 0}]
 
 for race in race_list:
-    distance_record = race['Distance']
     seconds_held = 0
 
     while seconds_held < race['Time']:
-        if seconds_held * (race['Time'] - seconds_held) >= distance_record:
+        if seconds_held * (race['Time'] - seconds_held) >= race['Distance']:
             race['Ways to beat'] += 1
         seconds_held += 1
 
@@ -17,12 +16,10 @@ print(math.prod(race['Ways to beat'] for race in race_list))
 submit(math.prod(race['Ways to beat'] for race in race_list), part='a')
 
 real_race = {'Time': 42686985, 'Distance': 284100511221341, 'Ways to beat': 0}
-
-distance_record = real_race['Distance']
 seconds_held = 0
 
 while seconds_held < real_race['Time']:
-    if seconds_held * (real_race['Time'] - seconds_held) >= distance_record:
+    if seconds_held * (real_race['Time'] - seconds_held) >= real_race['Distance']:
         real_race['Ways to beat'] += 1
     seconds_held += 1
 
